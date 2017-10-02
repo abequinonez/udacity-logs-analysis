22fff1f9fccf865e6f06cab8c64322b20810b026
abequinonez <abequinonez@users.noreply.github.com>
2017-10-02 15:31:16 -0700

    Add first query

--- report.py
+++ report.py
@@ -16,9 +16,32 @@ def get_report(query):
     return report
 
 
-def print_report(report):
+def print_report(question, report):
     """Prints the report retrieved from the database."""
+    print(question)
     for article in report:
         # Get the article's title and views
         title, views = article[0], article[1]
-        print("\"{}\" — {} views".format(title, views))
+        print("    \"{}\" — {} views".format(title, views))
+
+# I learned about implicit string concatenation from the following
+# Stack Overflow discussion:
+# https://stackoverflow.com/questions/1874592/how-to-write-very-long-string-that-conforms-with-pep8-and-prevent-e501
+top_articles = ("1. What are the most popular three articles of all time?",
+                "SELECT articles.title, COUNT(*) AS views FROM articles, log "
+                "WHERE ('/article/' || articles.slug = log.path) AND "
+                "(log.status = '200 OK') GROUP BY articles.title "
+                "ORDER BY views DESC LIMIT 3;")
+
+# Add the question/query sets to a list.
+query_sets = [top_articles]
+
+# Pass each set's data into the appropriate functions.
+for i, query_set in enumerate(query_sets):
+    question, query = query_set[0], query_set[1]
+    report = get_report(query)
+
+    # Print a newline before every report after the first one.
+    if i > 0:
+        print()
+    print_report(question, report)
